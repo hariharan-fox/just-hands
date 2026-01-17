5a7484b886cf342d3230281a352a20ce9d6076b8
Fox Social <hellofoxsocial@gmail.com>
2026-01-17 09:05:25 +0000

    Turn this Into h6 (_for element <Logo>_)

--- src/components/shared/logo.tsx
+++ src/components/shared/logo.tsx
@@ -3,8 +3,12 @@ import { cn } from '@/lib/utils';
 
 export function Logo({ className }: { className?: string }) {
   return (
-    <Link href="/" className={cn("text-xl font-bold font-headline text-primary", className)} suppressHydrationWarning>
-      <span>ConnectSphere</span>
+    <Link
+      href="/"
+      className={cn("text-xl font-bold font-headline text-primary", className)}
+      suppressHydrationWarning
+    >
+      <h6>ConnectSphere</h6>
     </Link>
   );
 }
