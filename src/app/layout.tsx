c24a724ba755762edef5351cd223faecc15a737e
Fox Social <hellofoxsocial@gmail.com>
2026-01-17 08:55:45 +0000

    Try fixing this error: `Console Error: A tree hydrated but some attribut

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={cn(headlineFont.variable, bodyFont.variable)} suppressHydrationWarning>
+    <html lang="en" className={cn(headlineFont.variable, bodyFont.variable)}>
       <body className={cn('min-h-screen bg-background font-body text-foreground/90 antialiased')}>
         <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
           <Sidebar />
