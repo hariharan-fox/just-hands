0665859b558710744a736d2597848e7f786be153
Fox Social <hellofoxsocial@gmail.com>
2026-02-24 00:51:44 +0000

    Not Good Just Change Other serif Font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,9 +4,9 @@ import { cn } from '@/lib/utils';
 import { Toaster } from '@/components/ui/toaster';
 import Header from '@/components/layout/header';
 import Sidebar from '@/components/layout/sidebar';
-import { Playfair_Display, Inter } from 'next/font/google';
+import { Merriweather, Inter } from 'next/font/google';
 
-const headlineFont = Playfair_Display({
+const headlineFont = Merriweather({
   subsets: ['latin'],
   weight: '700',
   display: 'swap',
